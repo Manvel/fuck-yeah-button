6bab963354c557359a22b098f46f45546981ef24
Manvel Saroyan <saroyanm@gmail.com>
2018-12-12 00:29:14 +0100

    Added voices and touch support

--- public/js/button.js
+++ public/js/button.js
@@ -2,6 +2,8 @@
 
 const button = document.querySelector("button");
 
+const fyeas = ["sounds/f1.mp3", "sounds/f2.mp3", "sounds/f3.mp3", "sounds/f4.mp3"].map((file) => new Audio(file));
+
 function createListener(eventName, classname, action)
 {
   button.addEventListener(eventName, () =>
@@ -18,12 +20,19 @@ function createListener(eventName, classname, action)
   });
 }
 
+function getRandomInt(max)
+{
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 function clicked()
 {
-  
+  fyeas[getRandomInt(3)].play();
 }
 
 createListener("mousedown", "mousedown", true);
 createListener("mouseup", "mousedown");
+createListener("touchstart", "mousedown", true);
+createListener("touchend", "mousedown");
 
 },{}]},{},[1]);
