c52c5c6e2bf2aef1f95b3d203ed60f6bb5a6a553
Manvel Saroyan <saroyanm@gmail.com>
2018-12-16 17:21:07 +0100

    Added more Diego  fyeahs

--- theme/js/button.js
+++ theme/js/button.js
@@ -35,8 +35,6 @@ function createListener(eventName, classname, action)
   {
     if (action)
     {
-      console.log(eventName);
-      console.log("clicked");
       clicked();
       button.classList.add(classname);
     }
