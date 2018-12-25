fed9b44b1be38d30df54e8f94a9c22426cc6caaf
Manvel Saroyan <saroyanm@gmail.com>
2018-12-25 23:50:35 +0100

    Added social media buttons

--- config.js
+++ config.js
@@ -1,11 +1,35 @@
 const {readdirSync, readFileSync, writeFileSync} = require("fs");
 const path = require("path");
 
+const shareButtons = [
+  {
+    "name": "Facebook",
+    "image": "/images/social/facebook.svg",
+    "path": "https://www.facebook.com/sharer/sharer.php?u="
+  },
+  {
+    "name": "Twitter",
+    "image": "/images/social/twitter.svg",
+    "path": "https://twitter.com/home?status="
+  },
+  {
+    "name": "Google Plus",
+    "image": "/images/social/gplus.svg",
+    "path": "https://plus.google.com/share?url="
+  },
+  {
+    "name": "Linkedin",
+    "image": "/images/social/linkedin.svg",
+    "path": "https://www.linkedin.com/shareArticle?url=goog.com"
+
+  }
+];
+
 const templateData =
 {
   site: {
-    title: "Fuck Yeah Project",
-    countText: "Fuck Yeah Counter:"
+    countText: "Fuck Yeah Counter:",
+    shareButtons
   }
 };
 
