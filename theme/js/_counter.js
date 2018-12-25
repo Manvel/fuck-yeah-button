2d9eaf4126a4edaf6a775faf291e12896b1c9ec3
Manvel Saroyan <saroyanm@gmail.com>
2018-12-25 21:31:30 +0100

    Added counter

--- theme/js/_counter.js
+++ theme/js/_counter.js
@@ -1,15 +1,32 @@
 const countContainer = document.querySelector("#count");
 const countTextElem = document.querySelector("#count span");
 
+const getCounter = () =>
+{
+  fetch("https://ksy4fyawu4.execute-api.us-west-2.amazonaws.com/prod/counter")
+  .then((response) => response.json()).then((count) =>
+  {
+    countContainer.classList.add("loaded");
+    countTextElem.textContent = count;
+  });
+};
+
 const incrementCount = () =>
 {
+  if (!countContainer.classList.contains("loaded"))
+    return;
+
   const count = countTextElem.textContent;
   countTextElem.textContent = parseInt(count, 10) + 1;
+  fetch("https://ksy4fyawu4.execute-api.us-west-2.amazonaws.com/prod/counter/increase",
+        {method: "POST"});
 };
 
 const setContainerPosition = (top) =>
 {
   countContainer.style.top = top;
 }
+
+getCounter();
 
 module.exports = {incrementCount, setContainerPosition};
